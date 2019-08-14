02e36a290975347096af4e275d1c986daf9c6213
dejunqi2008 <dejunqi2008@gmail.com>
2019-08-13 19:57:19 -0700

    init

---
+++ tests/GoogleSearchTest.js
@@ -0,0 +1,26 @@
+
+module.exports = {
+    '@tags': ['google'],
+    'Google advanced search test': function (browser) {
+        const page = browser.page.googleAdvancedSearch();
+        page
+            .navigate()
+            .setQuery('Elon Musk')
+            .selectFilter('@languageDropdown', 'lang_it')
+            .selectFilter('@lastUpdateDropdown', 'm')
+            .pause(1000)
+            .search()
+            .pause(1000);
+
+        const resultsPageQuerySelector = `#searchform input[name="q"][value="Elon Musk"]`;
+        const resultsPageLanguageSelector = '[aria-label="Search Italian pages"]';
+        const resultsPagelastUpdateSelector = '[aria-label="Past month"]';
+
+        browser.assert.visible(resultsPageQuerySelector, 'UI: Elon Musk is set in the query input');
+        browser.assert.containsText(resultsPageLanguageSelector, 'Search Italian pages', 'UI: Language is set to Italian');
+        browser.assert.containsText(resultsPagelastUpdateSelector, 'Past month', 'UI: Last update is set to Past Month');
+        browser.saveScreenshot('tests_output/google.png');
+
+        browser.end();
+    }
+};
---
+++ tests/sampleTest.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "My first test case": function(browser) {
+        browser
+            .url('https://news.ycombinator.com/')
+            .waitForElementVisible('.hnname')
+            .assert.containsText('.hnname', 'Hacker News')
+            .end();
+    }
+};
